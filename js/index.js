d78b8bb485008633a893771f75b86d4912e51990
Gabriele-sousa <gabriele.sousa14@etec.sp.gov.br>
2023-05-23 12:16:57 -0300

    atl 3

--- js/index.js
+++ js/index.js
@@ -4,13 +4,21 @@ ctx.canvas.width = window.innerWidth;
 ctx.canvas.height = window.innerHeight;
 
 ctx.fillStyle="#FFCBF6";
+ctx.strokeStyle="#ff7fe9";
 ctx.arc(950, 400, 150, 0, Math.PI * 2);
 ctx.fill();
 ctx.stroke();
 
 ctx.beginPath();
-ctx.moveTo(880,270);
+ctx.moveTo(885,265);
 ctx.bezierCurveTo(850, 200, 710, 300, 800,400);
 ctx.stroke();
 ctx.fill();
+
+ctx.beginPath();
+ctx.moveTo(1100,380);
+ctx.bezierCurveTo(1175, 500, 1070, 500, 1060, 500);
+ctx.stroke();
+ctx.fill();
+
 
