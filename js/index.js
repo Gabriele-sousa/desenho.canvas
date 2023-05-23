e0dd5847c81749cb6879d9688364dd24cdd0878a
Gabriele-sousa <gabriele.sousa14@etec.sp.gov.br>
2023-05-23 11:33:55 -0300

    atulização 2

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
 const canvas = document.getElementById('kirby');
 const ctx = canvas.getContext('2d');
+ctx.canvas.width = window.innerWidth;
+ctx.canvas.height = window.innerHeight;
 
-ctx,fillStyle="#FFCBF6";
-ctx.arc(700, 300, 200, 0, Math.PI * 2);
+ctx.fillStyle="#FFCBF6";
+ctx.arc(950, 400, 150, 0, Math.PI * 2);
 ctx.fill();
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(880,270);
+ctx.bezierCurveTo(850, 200, 710, 300, 800,400);
+ctx.stroke();
+ctx.fill();
+
