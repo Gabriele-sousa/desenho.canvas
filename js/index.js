e4c2eee9c6c1c036740f3fb5e87dad8c955c0ab7
gabriele-sousa <gabriele.sousa14@etec.sp.gov.br>
2023-06-01 21:06:42 -0300

    atl6

--- js/index.js
+++ js/index.js
@@ -4,48 +4,48 @@ ctx.canvas.width = window.innerWidth;
 ctx.canvas.height = window.innerHeight;
 
 ctx.beginPath();
-ctx.moveTo(830, 210);
-ctx.lineTo(840, 200);
-ctx.stroke();
-
-ctx.beginPath();
+ctx.strokeStyle="#F1C200";
+ctx.fillStyle="#FFF118";
 ctx.moveTo(860, 195);
 ctx.lineTo(900, 160);
+ctx.fill();
 ctx.stroke();
 
-ctx.beginPath();
+
 ctx.moveTo(900, 160);
 ctx.lineTo(850, 160);
 ctx.stroke();
 
-ctx.beginPath();
 ctx.moveTo(850, 160);
 ctx.lineTo(830, 110);
 ctx.stroke();
 
-ctx.beginPath();
 ctx.moveTo(830, 110);
 ctx.lineTo(815, 160);
 ctx.stroke();
 
-ctx.beginPath();
 ctx.moveTo(815, 160);
 ctx.lineTo(770, 160);
-ctx.stroke()
+ctx.stroke();
 
-ctx.beginPath();
 ctx.moveTo(770, 160);
 ctx.lineTo(805, 195);
 ctx.stroke();
 
-ctx.beginPath();
 ctx.moveTo(805, 195);
 ctx.lineTo(790, 245);
 ctx.stroke();
 
-ctx.beginPath();
 ctx.moveTo(790, 245);
 ctx.lineTo(830, 210);
+ctx.stroke();
+
+ctx.moveTo(830, 210);
+ctx.lineTo(870, 243);
+ctx.stroke();
+
+ctx.moveTo(870, 243);
+ctx.lineTo(860, 195);;
 ctx.stroke();
 
 ctx.beginPath();
